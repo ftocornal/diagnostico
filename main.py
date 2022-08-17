a5371ff585a1baa6095cb710fadccf00887f154a
FRANCISCO TOCORNAL <ftocornal@uc.cl>
2022-08-16 22:09:08 -0400

    second function end

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ with urllib.request.urlopen("https://www.kaggle.com/datasets/prathamsharma123/fa
     data = json.loads(url.read().decode())
     print(data)
 
-/function to find the biggest RetweetCount on data json
 def first_function ():
     max_retweet_count = 0
     for i in data:
@@ -13,6 +12,12 @@ def first_function ():
             print(max_retweet_count)
     # no alcance por el timpo, cambie el computador, no sabia que se requeria tener lenguajes instalados como python para estas pruebas
     return max_retweet_count
+
+def second_function ():
+    max_tweet_users = 0
+    return max_tweet_users
+
+
 
     
 
